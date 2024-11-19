2457d80d74fa186c3d2a13df16c3678edb368046
keoniperez1 <kperezla@students.kennesaw.edu>
2024-11-19 18:59:17 -0500

    Update codefile.py

--- codefile.py
+++ codefile.py
@@ -1,4 +1,4 @@
-import tkinter 
+from tkinter import *
 
 import random
 
@@ -12,13 +12,59 @@ FOODCOLOR = 'FF0000'
 BACKGROUND_COLOR = '#FFFFFF'
 
 class Snake:
-    pass
+
+    def __init__(self):
+        self.body_size = BODYPARTS
+        self.coordinates = []
+        self.squares = []
+
+        for i in range (0, BODYPARTS):
+            self.coordinates.append([0, 0])
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x,y, x + SPACE_SIZE, y + SPACE_SIZE, fill= SNAKECOLOR, tag='snake')
+            self.squares.append(square)
 
 class Food:
-    pass
+    
+    def __init__(self):
+        x = random.randint(0, (GAME_WIDTH/SPACE_SIZE)-1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT/SPACE_SIZE)-1) * SPACE_SIZE
+        
+        self.coordinates = [x,y]
+        
+        canvas.create_oval(x,y,x + SPACE_SIZE, fill = FOODCOLOR, tag='FOOD')
 
-def next_turn():
-    pass
+def next_turn(snake, food):
+
+        x, y = snake.coordinates [0]
+
+        if direction == 'up':
+            y -= SPACE_SIZE
+        
+        elif direction == 'down':
+            y+= SPACE_SIZE
+        
+        elif direction == 'left':
+            x -= SPACE_SIZE
+
+        elif direction == 'right':
+            x += SPACE_SIZE
+
+        snake.coordinates.insert(0, (x,y))
+
+        square = canvas.create_rectangle(x, y, x  + SPACE_SIZE, y + SPACE_SIZE)
+
+        snake.squares.insert (0, square)
+
+        del snake.coordinates [-1]
+
+        canvas.delete(snake.squares [-1])
+
+        del snake.squares[-1]
+
+        window.after(SPEED, next_turn, snake, food)
+
 
 def change_dic(new_direction):
     pass
@@ -52,8 +98,13 @@ screen_height = window.winfo_screenheight()
 
 x = int((screen_width/2) - (window_width/2))
         
-y = (screen_height/2) - (window_height/2))
+y = int((screen_height/2) - (window_height/2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
+snake = Snake()
+food = Food()
+
+
+next_turn(snake, food)
 window.mainloop()
