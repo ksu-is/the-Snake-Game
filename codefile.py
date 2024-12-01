fa94070ccd44031cd8c0d65fdd71bb7708d5c311
keoniperez1 <kperezla@students.kennesaw.edu>
2024-11-30 21:29:48 -0500

    Updates to code

--- codefile.py
+++ codefile.py
@@ -37,91 +37,91 @@ class Food:
 
 def next_turn(snake, food):
 
-    x, y = snake.coordinates[0]
+        x, y = snake.coordinates[0]
 
-    if direction == 'up':
-            y -= SPACE_SIZE
-        
-    elif direction == 'down':
-            y+= SPACE_SIZE
-        
-    elif direction == 'left':
-            x -= SPACE_SIZE
-
-    elif direction == 'right':
-            x += SPACE_SIZE
-
-    snake.coordinates.insert(0, (x, y))
-
-    square = canvas.create_rectangle(x, y, x  + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKECOLOR)
-
-    snake.squares.insert(0, square)
-
-    if x == food.coordinates[0] and y == food.coordinates[1]:
-
-        global score
-
-        score += 1 
-
-        label.config(text="Score:{}".format(score))
-
-        canvas.delete("food")
-
-        food = Food()
-
-    else:
+        if direction == 'up':
+                y -= SPACE_SIZE
             
-        del snake.coordinates[-1]
+        elif direction == 'down':
+                y+= SPACE_SIZE
+            
+        elif direction == 'left':
+                x -= SPACE_SIZE
 
-        canvas.delete(snake.squares[-1])
+        elif direction == 'right':
+                x += SPACE_SIZE
 
-        del snake.squares[-1]
-        
-    if check_collisions(snake):
-        gameover()
+        snake.coordinates.insert(0, (x, y))
 
-    else:
-        window.after(SPEED, next_turn, snake, food)
+        square = canvas.create_rectangle(x, y, x  + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKECOLOR)
+
+        snake.squares.insert(0, square)
+
+        if x == food.coordinates[0] and y == food.coordinates[1]:
+
+            global score
+
+            score += 1 
+
+            label.config(text="Score:{}".format(score))
+
+            canvas.delete("food")
+
+            food = Food()
+
+        else:
+                
+            del snake.coordinates[-1]
+
+            canvas.delete(snake.squares[-1])
+
+            del snake.squares[-1]
+            
+        if check_collisions(snake):
+            gameover()
+
+        else:
+            window.after(SPEED, next_turn, snake, food)
 
 
 def change_dic(new_direction):
 
-    global direction
+        global direction
 
-    if new_direction =='left':
-        if direction != 'right':
-            direction = new_direction
-    elif new_direction =='right':
-        if direction != 'left':
-            direction = new_direction
-    elif new_direction =='up':
-        if direction != 'down':
-            direction = new_direction
-    elif new_direction =='down':
-        if direction != 'up':
-            direction = new_direction
+        if new_direction =='left':
+            if direction != 'right':
+                direction = new_direction
+        elif new_direction =='right':
+            if direction != 'left':
+                direction = new_direction
+        elif new_direction =='up':
+            if direction != 'down':
+                direction = new_direction
+        elif new_direction =='down':
+            if direction != 'up':
+                direction = new_direction
 
 def check_collisions():
 
-    x, y = snake.coordinates[0]
+        x, y = snake.coordinates[0]
 
-    if x < 0 or x >= GAME_WIDTH:
-        return True
-    
-    elif y < 0 or y >= GAME_HEIGHT:
-        return True
-    
-    for body_part in snake.coordinates[1:]:
-        if x == body_part[0] and y == body_part [1]:
-            print("GAME OVER")
+        if x < 0 or x >= GAME_WIDTH:
             return True
-    
-    return False
+        
+        elif y < 0 or y >= GAME_HEIGHT:
+            return True
+        
+        for body_part in snake.coordinates[1:]:
+            if x == body_part[0] and y == body_part [1]:
+                print("GAME OVER")
+                return True
+        
+        return False
 
 def gameover():
-    canvas.delete(ALL)
-    canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/2
-                        ,font=('consolas', 70), text="GAME OVER", fill="red", tag="gameover")
+        canvas.delete(ALL)
+        canvas.create_text(canvas.winfo_width()//2, canvas.winfo_height()//2
+                            ,font=('consolas', 70), text="GAME OVER", fill="red", tag="gameover")
 
 window = Tk()
 window.title('Snake Game')
--- roadmap.md
+++ roadmap.md
@@ -5,3 +5,5 @@
 5. define functions
 6. make the code work as expected
 7. finish and test
+8.Create Final Code
+9. Had to redo code from other coding sources since I was unable to fix a problem that was preventing the code from running.
